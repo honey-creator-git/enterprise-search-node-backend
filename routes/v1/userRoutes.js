f06a9020499327f8afef675e72e539af2ce8fa82
HoneyCreator <114096873+honey-creator-git@users.noreply.github.com>
2024-11-13 14:24:33 -0700

    Update controller to save user

--- routes/v1/userRoutes.js
+++ routes/v1/userRoutes.js
@@ -2,20 +2,21 @@ const express = require("express");
 const documentController = require("../../controllers/v1/documentController");
 const setRoleMiddleware = require("../../middleware/setRoleMiddleware");
 const checkAdminAccess = require("../../middleware/checkAdminAccess");
+const checkViewerAccess = require("../../middleware/checkViewerAccess");
 const router = express.Router();
 
 router.post(
   "/",
   setRoleMiddleware,
-  checkAdminAccess,
+  checkViewerAccess,
   documentController.decodeUserTokenAndSave
 );
 
 router.get(
-    "/",
-    setRoleMiddleware,
-    checkAdminAccess,
-    documentController.getAllUsersFromTenant
-)
+  "/",
+  setRoleMiddleware,
+  checkAdminAccess,
+  documentController.getAllUsersFromTenant
+);
 
 module.exports = router;
